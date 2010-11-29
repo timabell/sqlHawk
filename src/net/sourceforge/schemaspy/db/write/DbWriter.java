ebb2a88f4a4e3e7d89be6c886162970e09e8112f
Tim Abell <tim@timwise.co.uk>
2010-11-29 12:29:24 +0000

    Proc deletion implemented.
    Output status info.

--- src/net/sourceforge/schemaspy/db/write/DbWriter.java
+++ src/net/sourceforge/schemaspy/db/write/DbWriter.java
@@ -16,10 +16,13 @@ public class DbWriter {
 	public void write(Config config, Connection connection,
 			DatabaseMetaData meta, String dbName, String schema,
 			Properties properties, Database db, Database existingDb) throws Exception {
+		System.out.println();
+		System.out.println("Updating existing database...");
 		//add/update stored procs.
 		Map<String, Procedure> existingProcs = existingDb.getProcMap();
 		for (Procedure updatedProc : db.getProcs()){
 			if (existingProcs.containsKey(updatedProc.getName())) {
+				System.out.println("Updating existing proc " + updatedProc.getName());
 				//TODO: check if definitions match
 				//Change definition from CREATE to ALTER and run.
 				if (!updatedProc.getDefinition().startsWith("CREATE"))
@@ -27,7 +30,15 @@ public class DbWriter {
 				String updateSql = updatedProc.getDefinition().replaceFirst("^CREATE", "ALTER");
 				connection.prepareStatement(updateSql).execute();
 			} else { //new proc
+				System.out.println("Adding new proc " + updatedProc.getName());
 				connection.prepareStatement(updatedProc.getDefinition()).execute();
+			}
+		}
+		Map<String, Procedure> updatedProcs = db.getProcMap();
+		for (Procedure existingProc : existingProcs.values()){
+			if (!updatedProcs.containsKey(existingProc.getName())){
+				System.out.println("Dropping unwanted proc " + existingProc.getName());
+				connection.prepareStatement("DROP PROCEDURE " + existingProc.getName()).execute();
 			}
 		}
 		//TODO: delete any unexpected procs
