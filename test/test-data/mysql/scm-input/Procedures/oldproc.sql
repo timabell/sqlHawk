4c0a0c0fe6c772bac5bf649cd406062c29f38b7a
Tim Abell <tim@timwise.co.uk>
2012-06-09 15:34:31 +0100

    mysql - failing test for altering an existing procedure

---
+++ test/test-data/mysql/scm-input/Procedures/oldproc.sql
@@ -0,0 +1,3 @@
+ALTER PROCEDURE testproc ()
+
+select 5; -- updated definition
--- test/test-data/mysql/setup.sql
+++ test/test-data/mysql/setup.sql
@@ -14,4 +14,11 @@ GO
 create user 'sqlhawktesting'@'localhost' identified by 'sqlhawktesting';
 GO
 grant all on sqlhawktesting.* to 'sqlhawktesting'@'localhost';
+GO
+
+-- set up some existing schema that will be modified
+
+CREATE PROCEDURE sqlhawktesting.oldproc ()
+
+select 4;
 
