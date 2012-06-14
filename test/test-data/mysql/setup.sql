b76f0e5d0155d2f7993c7bae6b6df4c4aae9a87f
Tim Abell <tim@timwise.co.uk>
2012-06-14 23:47:55 +0100

    mysql integration tests - added test tables from sakila

--- test/test-data/mysql/setup.sql
+++ test/test-data/mysql/setup.sql
@@ -11,6 +11,8 @@ GO
 
 create database sqlhawktesting;
 GO
+use sqlhawktesting;
+GO
 create user 'sqlhawktesting'@'localhost' identified by 'sqlhawktesting';
 GO
 grant all on sqlhawktesting.* to 'sqlhawktesting'@'localhost';
@@ -18,19 +20,50 @@ GO
 
 -- set up some existing schema that will be modified
 
-CREATE PROCEDURE sqlhawktesting.oldproc ()
+-- procedures
+
+CREATE PROCEDURE oldproc ()
 
 select 4;
 
 GO
 
-CREATE PROCEDURE sqlhawktesting.unchangedproc ()
+CREATE PROCEDURE unchangedproc ()
 
 select 5;
 
 GO
 
-CREATE PROCEDURE sqlhawktesting.unwantedproc ()
+CREATE PROCEDURE unwantedproc ()
 
 select 6;
-
+GO
+-- tables (extracts from mysql example database sakila)
+CREATE TABLE `language` (
+  `language_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `name` char(20) NOT NULL,
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`language_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+GO
+CREATE TABLE `film` (
+  `film_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` text,
+  `release_year` year(4) DEFAULT NULL,
+  `language_id` tinyint(3) unsigned NOT NULL,
+  `original_language_id` tinyint(3) unsigned DEFAULT NULL,
+  `rental_duration` tinyint(3) unsigned NOT NULL DEFAULT '3',
+  `rental_rate` decimal(4,2) NOT NULL DEFAULT '4.99',
+  `length` smallint(5) unsigned DEFAULT NULL,
+  `replacement_cost` decimal(5,2) NOT NULL DEFAULT '19.99',
+  `rating` enum('G','PG','PG-13','R','NC-17') DEFAULT 'G',
+  `special_features` set('Trailers','Commentaries','Deleted Scenes','Behind the Scenes') DEFAULT NULL,
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`film_id`),
+  KEY `idx_title` (`title`),
+  KEY `idx_fk_language_id` (`language_id`),
+  KEY `idx_fk_original_language_id` (`original_language_id`),
+  CONSTRAINT `fk_film_language` FOREIGN KEY (`language_id`) REFERENCES `language` (`language_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_film_language_original` FOREIGN KEY (`original_language_id`) REFERENCES `language` (`language_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1001 DEFAULT CHARSET=utf8;
